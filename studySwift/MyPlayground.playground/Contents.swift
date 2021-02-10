fc7b55e334831096209d679f5e82aff1c0bc1791
wltn2814 <wltn102998@gmail.com>
2021-02-11 02:28:21 +0900

    create studyiOS repository

---
+++ studySwift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+var str2 : String = "Hello, playground"
+
+let strNonedit : String = "Hello, playground"
+
+var intTest : Int = 10
+var floatTest : Float = 10.5
+var doubleTest : Double = 10.0
+var characterTest : Character = "H"
+var stringTest : String = "Hello, playground"
